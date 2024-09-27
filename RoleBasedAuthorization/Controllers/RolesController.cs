b8f7223a453dcc36beded5e1d0e10a52ea599838
vasiliv <32122468+vasiliv@users.noreply.github.com>
2024-09-27 10:58:47 +0400

    Add project files.

---
+++ RoleBasedAuthorization/Controllers/RolesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoleBasedAuthorization.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _manager;
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _manager = roleManager;       
+        }
+        public IActionResult Index()
+        {
+            var roles = _manager.Roles;
+            return View();
+        }
+        public IActionResult Create()
+        {
+            var roles = _manager.Roles;
+            return View();
+        }
+        [HttpPost
+            ]
+        public IActionResult Create(IdentityRole role)
+        {
+            //check if role exists
+            if (!_manager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
+            {
+                _manager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
